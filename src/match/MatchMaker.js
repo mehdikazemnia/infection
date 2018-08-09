0bca31fcf4e5c0b029c252031af6a95f024ea910
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-08-09 11:44:12 +0430

    match maker file basics and todos

---
+++ src/match/MatchMaker.js
@@ -0,0 +1,15 @@
+const Match = require('./Match');
+
+function make(settings) {
+
+    // TODOS:
+    // remove existing match (if exists)
+    // load the map by settings.mapId
+    // make a new Match based on the map
+    // ...
+
+}
+
+module.exports = {
+    make: make
+};
