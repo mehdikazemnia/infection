333aaa92342aef835d3eedf0990e9299267bd897
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-08-09 13:16:55 +0430

    pixi application created

--- src/match/Match.js
+++ src/match/Match.js
@@ -9,7 +9,19 @@ class Match extends Map {
         // init the map
         super({ size: settings.size });
 
+        this.pixiapp = new PIXI.Application({
+            width: settings.size[0],
+            height: settings.size[1],
+            transparent: true
+        });
+
+        // TODO: load textures
+        // this.pixi.loader
+        //     .add('...', '...')
+        //     .load(() => { this.onLoad() });
     }
+
+    onLoad() {}
 
     makeCell(settings) {
         let cell = new Cell(settings);
