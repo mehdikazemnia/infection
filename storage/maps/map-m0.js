32eed370dbc56a612ea224f4d34cbf999516d6f5
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-08-09 12:15:02 +0430

    first map info

---
+++ storage/maps/map-m0.js
@@ -0,0 +1,182 @@
+module.exports = {
+    id: 'm0',
+    size: [1000, 600],
+    capacity: 2,
+    cells: [
+        {
+            pos: { x: 50, y: 50 },
+            capacity: 150,
+            population: 20,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 10, y: 30 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 20, y: 20 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 30, y: 15 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 40, y: 15 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 50, y: 20 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 60, y: 15 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 70, y: 15 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 80, y: 20 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 90, y: 30 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 10, y: 70 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 20, y: 80 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 30, y: 85 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 40, y: 85 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 50, y: 80 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 60, y: 85 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 70, y: 85 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 80, y: 80 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 90, y: 70 },
+            capacity: 50,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 80, y: 50 },
+            capacity: 200,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 65, y: 35 },
+            capacity: 100,
+            population: 1,
+            owner: 'p0',
+            type: 'normal'
+        },
+        {
+            pos: { x: 65, y: 65 },
+            capacity: 100,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 20, y: 50 },
+            capacity: 200,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 35, y: 35 },
+            capacity: 100,
+            population: 1,
+            owner: false,
+            type: 'normal'
+        },
+        {
+            pos: { x: 35, y: 65 },
+            capacity: 100,
+            population: 1,
+            owner: 'p2',
+            type: 'normal'
+        }
+    ]
+}
