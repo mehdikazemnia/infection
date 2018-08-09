64397b5d801ec8fdc7a8f3105642f35b1d89e5f7
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-08-09 12:15:24 +0430

    Match basic methods

--- src/match/Match.js
+++ src/match/Match.js
@@ -1,6 +1,23 @@
 const Map = require('red-steer').Map;
+const Cell = require('./Cell');
+const Virus = require('./Virus');
 
 class Match extends Map {
+
+    constructor(settings) {
+
+        // init the map
+        super({ size: settings.size });
+
+    }
+
+    makeCell(settings) {
+        let cell = new Cell(settings);
+    }
+
+    makeVirus(settings) {
+        let virus = new Virus(settings);
+    }
 
 }
 
