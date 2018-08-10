6bcbb33fb0f2771be73f8ed717abd17ad8fd32ce
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-08-10 19:44:31 +0430

    accurate positions instead of percent

--- storage/maps/map-m0.js
+++ storage/maps/map-m0.js
@@ -4,175 +4,175 @@ module.exports = {
     capacity: 2,
     cells: [
         {
-            pos: { x: 50, y: 50 },
+            pos: { x: 500, y: 300 },
             capacity: 150,
             population: 20,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 10, y: 30 },
+            pos: { x: 100, y: 180 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 20, y: 20 },
+            pos: { x: 200, y: 120 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 30, y: 15 },
+            pos: { x: 300, y: 90 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 40, y: 15 },
+            pos: { x: 400, y: 90 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 50, y: 20 },
+            pos: { x: 500, y: 120 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 60, y: 15 },
+            pos: { x: 600, y: 90 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 70, y: 15 },
+            pos: { x: 700, y: 90 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 80, y: 20 },
+            pos: { x: 800, y: 120 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 90, y: 30 },
+            pos: { x: 900, y: 180 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 10, y: 70 },
+            pos: { x: 100, y: 420 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 20, y: 80 },
+            pos: { x: 200, y: 480 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 30, y: 85 },
+            pos: { x: 300, y: 510 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 40, y: 85 },
+            pos: { x: 400, y: 510 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 50, y: 80 },
+            pos: { x: 500, y: 480 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 60, y: 85 },
+            pos: { x: 600, y: 510 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 70, y: 85 },
+            pos: { x: 700, y: 510 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 80, y: 80 },
+            pos: { x: 800, y: 480 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 90, y: 70 },
+            pos: { x: 900, y: 420 },
             capacity: 50,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 80, y: 50 },
+            pos: { x: 800, y: 300 },
             capacity: 200,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 65, y: 35 },
+            pos: { x: 630, y: 210 },
             capacity: 100,
             population: 1,
             owner: 'p0',
             type: 'normal'
         },
         {
-            pos: { x: 65, y: 65 },
+            pos: { x: 630, y: 390 },
             capacity: 100,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 20, y: 50 },
+            pos: { x: 200, y: 300 },
             capacity: 200,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 35, y: 35 },
+            pos: { x: 370, y: 210 },
             capacity: 100,
             population: 1,
             owner: false,
             type: 'normal'
         },
         {
-            pos: { x: 35, y: 65 },
+            pos: { x: 370, y: 390 },
             capacity: 100,
             population: 1,
             owner: 'p2',
